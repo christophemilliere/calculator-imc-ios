f0e69346a457159f72c871c520b45f1d16261bb1
christophemilliere <christophemilliere93@gmail.com>
2017-11-04 22:38:11 +0100

    add stepper

--- calculatrice-imc/ViewController.swift
+++ calculatrice-imc/ViewController.swift
@@ -9,14 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var valueSize:Int?
+    var valueKilo:Int?
 
     @IBOutlet weak var textFieldSize: UITextField!
     @IBOutlet weak var textFieldKilo: UITextField!
+    @IBOutlet weak var labelResponseImc: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func calculator(_ sender: UIButton) {
+        calvulatorImc()
+    }
+    
+    @IBAction func incrementSizeAction(_ sender: UIStepper) {
+        valueSize = Int(sender.value)
+        textFieldSize.text! = "\(valueSize!)"
+    }
+    
+    @IBAction func incrementKiloAction(_ sender: UIStepper) {
+        valueKilo = Int(sender.value)
+        textFieldKilo.text! = "\(valueKilo!)"
+    }
+    
+    func calvulatorImc(){
+        let taille2 = Double(valueSize!) / 100 * Double(valueSize!) / 100
+        let imc = Double(valueKilo!) / taille2
+        labelResponseImc.text! = "\(interpreterIMC(imc: imc))"
+    }
+    
+    func interpreterIMC(imc:Double) -> String {
+        var text = " avec un IMC de \(imc)"
 
+        switch imc {
+        case 0...16:
+            text = "Dénutrition \(text)"
+        case 16.5...18.5:
+            text = "État de maigreur\(text)"
+        case 18.5...25:
+            text = "Corpulence normale\(text)"
+        case 25...30:
+            text = "Surpoids\(text)"
+        case 30...35:
+            text = "Obésité modéré\(text)"
+        case 35...40:
+            text = "Obèsité sévère\(text)"
+        case 40...50:
+            text = "Obèsité morbide\(text)"
+        default:
+            print("")
+        }
+        return text
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
