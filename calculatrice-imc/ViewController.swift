a24f6a4a574edb0bec89f3e4ccb1b56a5bc9d6a0
christophemilliere <christophemilliere93@gmail.com>
2017-11-04 12:23:07 +0100

    Initial Commit

---
+++ calculatrice-imc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculatrice-imc
+//
+//  Created by christophe milliere on 04/11/2017.
+//  Copyright © 2017 christophe milliere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
