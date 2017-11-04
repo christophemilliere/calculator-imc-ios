8c4f20e5b2ecd8a2c1da3ed54db0c13718354381
christophemilliere <christophemilliere93@gmail.com>
2017-11-04 16:59:24 +0100

    add iBoulet

--- calculatrice-imc/ViewController.swift
+++ calculatrice-imc/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldSize: UITextField!
+    @IBOutlet weak var textFieldKilo: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
